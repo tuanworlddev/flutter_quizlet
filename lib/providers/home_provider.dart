030251e65c29186e85c9a48d054648aedbf0b6ac
tuanworlddev <nanhtuan.it@gmail.com>
2025-05-08 10:31:22 +0700

    update home screen

---
+++ lib/providers/home_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quizlet/models/course_model.dart';
+import 'package:flutter_quizlet/services/course_service.dart';
+
+class HomeProvider with ChangeNotifier {
+  final CourseService _courseService = CourseService();
+  List<CourseModel> _courses = [];
+
+  List<CourseModel> get course => _courses;
+
+  void streamCourses() {
+    _courseService.streamAllCourses().listen((courses) {
+      _courses = courses;
+      notifyListeners();
+    });
+  }
+}
